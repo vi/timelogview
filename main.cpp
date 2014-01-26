f18da058c685b3ede53ee14b39b4e00de3f96b39
Vitaly _Vi Shukela <vi0oss@gmail.com>
2014-01-27 00:02:01 +0300

    Unhardcode input file name

--- main.cpp
+++ main.cpp
@@ -10,8 +10,20 @@ int main(int argc, char *argv[])
     QVector<LogEntry> *logs = new QVector<LogEntry>();
     logs->reserve(1000);
 
-    FILE* f = fopen("input.txt", "rt");
-    assert(f || !"input.txt");
+    const char* fn = "-";
+    FILE* f = stdin;
+
+    if(getenv("F")) fn = getenv("F");
+
+    if(QString(fn) != "-") {
+        f = fopen(fn, "rt");
+        assert(f);
+    } else {
+        fprintf(stderr, "Reading logs from stdin until EOF\n");
+        fprintf(stderr, "Each line must begin with monotonously increasing number (timestamp)\n");
+        fflush(stderr);
+    }
+
     for(;;) {
         char buf[2048];
         if(!fgets(buf, sizeof buf, f)) break;
